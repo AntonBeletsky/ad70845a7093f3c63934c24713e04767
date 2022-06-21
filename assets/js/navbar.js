a5e22fc3406e66b5a4b1e82a777551a5030a8bbd
Anton Beletsky <AntonBeletskyEU@gmail.com>
2022-06-22 00:45:20 +0300

    itopnav
    itopnav- mytopnav

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,8 +1,6 @@
-
 /* <!-- <script src="https://kit.fontawesome.com/ad126f550b.js" crossorigin="anonymous"><   -->*/
 
 /*<!-- РЕСПОНГСИВНОЕ МЕНЮ n -->*/
-
 
 /* Gamburger */
 
@@ -10,59 +8,50 @@ let screen_mode = "desk"; /*  "smart" */
 const width_param = 600;
 
 function screenModeSwither() {
-    if (screen_mode == "smart") {
-        $(".responsive").removeClass("responsive");
-        $(".hide-responsive").css("display", "none");
-        screen_mode = "desk";
-        /*alert('hide');*/
-    }
-    else {
-        $("#myTopnav.topnav").addClass("responsive"); /* широкие кнопки */
-        $(".hide-responsive").css("display", "block");
-        screen_mode = "smart";
-        /*alert('show');*/
-    }
-
+  if (screen_mode == "smart") {
+    $(".responsive").removeClass("responsive");
+    $(".hide-responsive").css("display", "none");
+    screen_mode = "desk";
+    /*alert('hide');*/
+  } else {
+    $("#itopnav.topnav").addClass("responsive"); /* широкие кнопки */
+    $(".hide-responsive").css("display", "block");
+    screen_mode = "smart";
+    /*alert('show');*/
+  }
 }
 
 /* возващает меню что б не пропадало  */
 
 $(window).resize(function () {
+  doc_width = document.body.clientWidth; /* $(document).width() dont work  */
 
-    doc_width = document.body.clientWidth; /* $(document).width() dont work  */
-
-
-    if ((doc_width > width_param) && (screen_mode == "desk")) {
-        $(".responsive").removeClass("responsive");
-        $(".hide-responsive").css("display", "block");
-        screen_mode = "desk";
-        console.log('remove respons');
-    }
-    if ((doc_width <= width_param) && (screen_mode == "desk")) {
-        $(".responsive").removeClass("responsive");
-        $(".hide-responsive").css("display", "none");
-        screen_mode = "desk";
-        console.log('remove respons');
-
-    }
+  if (doc_width > width_param && screen_mode == "desk") {
+    $(".responsive").removeClass("responsive");
+    $(".hide-responsive").css("display", "block");
+    screen_mode = "desk";
+    console.log("remove respons");
+  }
+  if (doc_width <= width_param && screen_mode == "desk") {
+    $(".responsive").removeClass("responsive");
+    $(".hide-responsive").css("display", "none");
+    screen_mode = "desk";
+    console.log("remove respons");
+  }
 });
 
 /* при переходе в смарт короткие кнопки , а это делает широкие исправлено doc_width */
-function smart_fix()
-{
-    $("#myTopnav.topnav").addClass("responsive"); /* широкие кнопки */
-    $(".hide-responsive").css("display", "block");
-    screen_mode = "smart";
+function smart_fix() {
+  $("#itopnav.topnav").addClass("responsive"); /* широкие кнопки */
+  $(".hide-responsive").css("display", "block");
+  screen_mode = "smart";
 }
 
 $(window).resize(function () {
-    
-    if (doc_width <= width_param)  {
-        // smart_fix();
-    }
+  if (doc_width <= width_param) {
+    // smart_fix();
+  }
 });
-
-
 
 /* SIDEBAAAAAAAAAR*/
 
@@ -71,20 +60,18 @@ $(window).resize(function () {
 */
 
 function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
 
-    console.log("openNav");
-    /*document.getElementById("myTopnav").style.marginLeft = "250px";*/
-
+  console.log("openNav");
+  /*document.getElementById("itopnav").style.marginLeft = "250px";*/
 }
 
 function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
 
-    document.getElementById("myTopnav").style.marginLeft = "0px";
+  document.getElementById("itopnav").style.marginLeft = "0px";
 
-    console.log("closeNav");
+  console.log("closeNav");
 }
-
