99bd31073a8eaf6c1fda1bd47157b91a8364ec1a
Anton Beletsky <AntonBeletskyEU@gmail.com>
2022-07-10 05:06:07 +0300

    search

--- assets/js/search-dropbox.js
+++ assets/js/search-dropbox.js
@@ -1,11 +1,14 @@
 const search_len_drop_datalist_param = 3;
 
 $("#search-input").keyup(function () {
-  if ($(this).val().length >= search_len_drop_datalist_param) {
+  /* console.log("ku"); */
+  if ($("#search-input").val().length >= search_len_drop_datalist_param) {
     // Enable
     $("datalist#search-datalist-disable").attr("id", "search-datalist-enabled");
+    /*console.log("enable");*/
   } else {
     // Disable
-    $("datalist#search-datalist-disable").attr("id", "search-datalist-disable");
+    $("datalist#search-datalist-enabled").attr("id", "search-datalist-disable");
+    /*console.log("disable");*/
   }
 });
