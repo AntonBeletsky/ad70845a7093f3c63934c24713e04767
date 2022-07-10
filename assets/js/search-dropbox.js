e34273efa6991c8d4c08e4c4d005306a86d8dfa4
Anton Beletsky <AntonBeletskyEU@gmail.com>
2022-07-10 12:02:38 +0300

    1

--- assets/js/search-dropbox.js
+++ assets/js/search-dropbox.js
@@ -1,5 +1,5 @@
 const search_len_drop_datalist_param = 3;
-
+/* TODO: browser detect is as key malware  */
 $("#search-input").on("input", function () {
   /* console.log("ku"); */
   if ($("#search-input").val().length >= search_len_drop_datalist_param) {
