bd3dc004d83cf5aa279dbda4849d4e2fb724c609
Anton Beletsky <AntonBeletskyEU@gmail.com>
2022-09-15 01:43:33 +0300

    zar

--- assets/js/product_page/product-page-product-info/product-info-filters.js
+++ assets/js/product_page/product-page-product-info/product-info-filters.js
@@ -1,13 +1,16 @@
 /* product filters buttons classes click events */
-
+/*
 $(".pif-btn-default").click(function () {
   console.log(".pif-btn-default");
   console.log(this);
 });
+*/
 
+/*
 $(".pif-btn-active").click(function () {
   console.log(".pif-btn-active");
 });
+*/
 
 $("div.pif-btn-default").click(function () {
   /* remove old active btn */
@@ -18,6 +21,10 @@ $("div.pif-btn-default").click(function () {
     .addClass("pif-btn-default");
 
   /* input*/
+  $(".product-info-filters-filter-control > input[checked]").removeAttr(
+    "checked"
+  );
+
   $($(".product-info-filters-filter-control > input[checked]")[0]).removeAttr(
     "checked"
   );
@@ -29,6 +36,6 @@ $("div.pif-btn-default").click(function () {
 
   $(this).removeClass("pif-btn-default").addClass("pif-btn-active");
   /* input */
-  /*$(this).parent().find("input").attr("checked", "checked");*/
+  $(this).parent().find("input").attr("checked", "checked");
   $(this).parent().find("input").checked = true;
 });
