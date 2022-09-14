02e7ebb0f572f7bfb5d26d6e27ca34dfd9be69d6
Anton Beletsky <AntonBeletskyEU@gmail.com>
2022-09-15 01:26:54 +0300

    12

--- assets/js/product_page/product-page-product-info/product-info-filters.js
+++ assets/js/product_page/product-page-product-info/product-info-filters.js
@@ -1,44 +1,36 @@
-/* buttons classes click events */
+/* product filters buttons classes click events */
 
-/* select filter */
-/*
-определить кнопку которую кликнули
-не дать кликнуть дизаблед
-запомнить зис
-поставить акт на деф
-зис на акт
-
-поменять заголовок 
-*/
+$(".pif-btn-default").click(function () {
+  console.log("div.pif-btn-default");
+});
 
 $("div.pif-btn-default").click(function () {
-  /* input = $($(this)[0]).prev()[0];*/
-  /*console.log($($(this)[0]).prev()[0]);*/
-  /* div .btn*/
-  /*console.log($($(this)[0])[0]);*/
-
   /* remove old active btn */
+
   /* div */
-  $($(".product-info-filters-filter-control > div.pif-btn-active")[0])
-    .removeClass("pif-btn-active")
-    .addClass("pif-btn-default");
+  $(".product-info-filters-filter-control > div.pif-btn-active").addClass(
+    "pif-btn-default"
+  );
+  $(".product-info-filters-filter-control > div.pif-btn-active").removeClass(
+    "pif-btn-active"
+  );
+
   /* input*/
   $($(".product-info-filters-filter-control > input[checked]")[0]).removeAttr(
     "checked"
   );
 
+  /************************************************************* */
+
   /* select new filter (radio button ) */
   /*div btn*/
+
   $(this).removeClass("pif-btn-default").addClass("pif-btn-active");
   /* input */
-  $($(this)).prev().attr("checked", "checked");
-
-  /*
-  console.log($(this));
-  console.log("def");
-  */
+  /*$(this).parent().find("input").attr("checked", "checked");*/
+  $(this).parent().find("input").checked = true;
 });
 
 $("div.pif-btn-active").click(function () {
-  /*console.log("active lol kurwa");*/
+  console.log("div.pif-btn-active");
 });
