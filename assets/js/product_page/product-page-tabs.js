2df6d8497c76d5b61ee5b5bf588f871e8e4ba232
Anton Beletsky <AntonBeletskyEU@gmail.com>
2022-09-16 01:08:44 +0300

    tabs

--- assets/js/product_page/product-page-tabs.js
+++ assets/js/product_page/product-page-tabs.js
@@ -1,22 +1,24 @@
-function productPageOpenPage(pageName) {
-  /* hide all tabs */
+/*
+function FuckproductPageOpenPage(pageName) {
+  /* hide all tabs */ /*
   var i, tabcontent;
   tabcontent = document.getElementsByClassName("product-page-tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
-    console.log(tabcontent[i]);
   }
 
-  /* show clicked tab */
-  console.log(pageName);
+  /* show clicked tab */ /*
   document.getElementById(pageName).style.display = "block";
 }
 
 // Get the element with id="prodpagedefaultOpen" and click on it
 document.getElementById("prodPageDefaultOpen").click();
-
+*/
 /************************************************************************ */
 
-$("button[tabid]").click(function () {
-  console.log;
+/* open tabs*/
+$(".product-page-tabs button[tabid]").click(function () {
+  var attr = $(this).attr("tabid");
+  $(".product-page-tabcontent").css("display", "none");
+  $($(".product-page-tabs #" + attr)).css("display", "block");
 });
