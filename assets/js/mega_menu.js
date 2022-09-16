8e4b4924cea80494aa8cf106d6fac4f76952535c
Anton Beletsky <AntonBeletskyEU@gmail.com>
2022-09-16 19:43:19 +0300

    mega mnu

--- assets/js/mega_menu.js
+++ assets/js/mega_menu.js
@@ -1,15 +1,19 @@
-/* city - tabname TODO: rename */
+/*
+/* city - tabname TODO: rename */ /*
 function openCategory(evt, tabnum) {
   // Declare all variables
   var i, tabcontent, tablinks;
 
   // Get all elements with class="mega-menu-tabcontentt" and hide them
+  /*
   tabcontent = document.getElementsByClassName("mega-menu-tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
+  */
 
-  // Get all elements with class="tablinks" and remove the class "active"
+// Get all elements with class="tablinks" and remove the class "active"
+/*
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
@@ -26,3 +30,18 @@ function openCategory(evt, tabnum) {
 
   evt.currentTarget.className += " active";
 }
+*/
+
+/* open tabs*/
+$(".megamenu-tab button[megamenutabid]").hover(function () {
+  var attr = $(this).attr("megamenutabid");
+  /* hide all tabs */
+  $(".mega-menu-tabcontent").css("display", "none");
+  /* show selected tab */
+  $($(".mega-dropdown-content .megamenu-row #" + attr)).css("display", "block");
+
+  /* remove all keypress btn state */
+  $(".megamenu-tab button[megamenutabid]").removeClass("active");
+  /* backcolor for btn */
+  $(this).addClass("active");
+});
